26abd9d8bd61ed551dabfbd15615598472701a0e
santiago197 <santinacional123.sr@gmail.com>
2026-06-10 17:41:50 -0500

    Refactor code structure for improved readability and maintainability

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      // Fix ESM "fully specified" resolution for MUI and react-transition-group
+      const oneOfRule = webpackConfig.module.rules.find((r) => r.oneOf);
+      if (oneOfRule) {
+        oneOfRule.oneOf.forEach((rule) => {
+          if (rule.resolve) {
+            rule.resolve.fullySpecified = false;
+          }
+        });
+      }
       webpackConfig.module.rules.push({
         test: /\.m?js$/,
         resolve: {
