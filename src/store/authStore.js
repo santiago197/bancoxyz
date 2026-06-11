03e9edef6a278d42f3f7de931e7c7ead9b5a116c
santiago197 <santinacional123.sr@gmail.com>
2026-06-11 08:08:06 -0500

    feat: integrate zustand middleware for persistent auth state management

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,8 +1,14 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useAuthStore = create((set) => ({
-  user: null,
-  token: null,
-  login: (user, token) => set({ user, token }),
-  logout: () => set({ user: null, token: null }),
-}));
+export const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      login: (user, token) => set({ user, token }),
+      logout: () => set({ user: null, token: null }),
+    }),
+    { name: 'bancoxyz_auth' }
+  )
+);
